8808722b8fbc1416bc72fd461188bbb91ad3c4a4
robin chaux <robinvchaux@gmail.com>
2024-07-09 18:37:42 +0200

    mini code R pour tester

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,14 +5,20 @@ class PagesController < ApplicationController
   def calculate
     require "rinruby"
 
+    # Les deux variables récupérées depuis le formulaire
     var1 = params[:var1].to_i
     var2 = params[:var2].to_i
 
-    R.eval <<RSCRIPT
+    # Un script R qui fait une simple addition
+    # Avec la définition de la méthode, comme en Ruby presque
+    # Puis l'utilisation de la méthode avec les deux variables mises dans l'objet res
+    # Fin du script
+    # R.pull du res pour pouvoir l'utiliser dans Ruby
+    R.eval <<-RSCRIPT
       simple_addition <- function(var1, var2) {
         return (var1 + var2)
       }
-      res <- simple_addition('{var1}, #{var2}')
+      res <- simple_addition(#{var1}, #{var2})
     RSCRIPT
 
     @result = R.pull("res")
