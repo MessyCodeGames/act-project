4e612c716e9aaf36fcd13f10aa5a74ca8b50e70b
Thanh Truc PHAM <167073331+trucpham97@users.noreply.github.com>
2024-07-25 15:37:59 +0200

    Modified small details of scroll to result section function
    Signed-off-by: Thanh Truc PHAM <167073331+trucpham97@users.noreply.github.com>

--- app/javascript/controllers/form_handler_controller.js
+++ app/javascript/controllers/form_handler_controller.js
@@ -99,8 +99,8 @@ export default class extends Controller {
           }
         }
       });
+      this.resultTarget.scrollIntoView({ behavior: 'smooth' });
     })
-    this.resultTarget.scrollIntoView({ behavior: 'smooth' });
   }
 
 
