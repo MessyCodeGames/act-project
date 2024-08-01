27fa3c216f26e5bd3783f2f13a78bd16a457c3cb
robin chaux <robinvchaux@gmail.com>
2024-08-01 10:19:28 +0200

    Added some smooth folding and unfolding for biblio

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -9,12 +9,27 @@ export default class extends Controller {
   }
 
   fire() {
-    this.togglableElementTarget.classList.toggle("hidden");
+    const element = this.togglableElementTarget;
+    const isExpanded = element.classList.contains("expanded");
+
+    if (isExpanded) {
+      element.style.height = `${element.scrollHeight}px`; // Set the height to the current scroll height
+      requestAnimationFrame(() => {
+        element.style.height = '0'; // Then set it to 0 to trigger the transition
+      });
+    } else {
+      element.style.height = `${element.scrollHeight}px`; // Set the height to the scroll height to trigger the transition
+      element.addEventListener('transitionend', () => {
+        element.style.height = 'auto'; // Remove the inline height after the transition ends
+      }, { once: true });
+    }
+
+    element.classList.toggle("expanded");
 
     this.buttonActiveTarget.classList.toggle("bg-light-color");
     this.buttonActiveTarget.classList.toggle("bg-white");
     this.buttonActiveTarget.classList.toggle("lg:hover:bg-very-light-color");
-    this.buttonActiveTarget.classList.toggle("scale-95")
-    this.buttonActiveTarget.classList.toggle("active:scale-90")
+    this.buttonActiveTarget.classList.toggle("scale-95");
+    this.buttonActiveTarget.classList.toggle("active:scale-90");
   }
 }
